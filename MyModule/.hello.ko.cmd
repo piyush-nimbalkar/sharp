543968c110b03a2e73f1442f7e208a2b9d4157ed
Harshad <harshadshirwadkar@gmail.com>
2010-07-17 11:52:59 +0800

    Added Module Linked List

--- MyModule/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/MyModule/hello.ko := ld -r -m elf_i386  --build-id -o /root/MyModule/hello.ko /root/MyModule/hello.o /root/MyModule/hello.mod.o
---
+++ kernel_modules/moduleLinkedList/moduleLinkedList.c
@@ -0,0 +1,48 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/slab.h>
+
+struct node
+{
+  int data;
+  struct node * next;
+}*start;
+
+MODULE_LICENSE("GPL");
+
+int init_module(void)
+{
+  int i;
+  struct node *p;
+  start=kmalloc(sizeof(struct node),GFP_KERNEL);
+  p=start;
+  start->data=0;
+  for(i=1;i<5;i++)
+    {
+      p->next=kmalloc(sizeof(struct node),GFP_KERNEL);
+      p=p->next;
+      p->data=i;
+    }
+  p->next=NULL;
+  return 0;
+}
+
+void cleanup_module(void)
+{
+  struct node * p;
+  p=start;
+  while(p!=NULL)
+    {
+      printk("%d ",p->data);
+      p=p->next;
+    }
+  p=start->next;
+  while(p!=NULL)
+    {
+      kfree(start);
+      start=p;
+      p=p->next;
+    }
+}
+
+
